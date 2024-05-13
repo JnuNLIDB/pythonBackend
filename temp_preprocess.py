9ded38576c9effccb72827d35099b2502f0a5d40
Fox_white <vct.xie@gmail.com>
2024-05-13 18:13:54 +0800

    Use chat model

--- temp_preprocess.py
+++ temp_preprocess.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.mkdir("data")
     with open("data/theory_preprocessed.txt", "w", encoding="utf-8") as f:
         for line in open(sys.argv[1], "r", encoding="utf-8"):
             j = json.loads(line)
